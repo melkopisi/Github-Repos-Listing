39b55aaed69d0efeb66b0f2d582eee5af65493e8
Kopisi <m.elkopisi@gmail.com>
2022-09-03 11:32:40 +0200

    added viewbinding and hilt implementation

---
+++ presentation/src/main/java/me/melkopisi/githubreposlisting/GithubApplication.kt
@@ -0,0 +1,16 @@
+package me.melkopisi.githubreposlisting
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/*
+ * Authored by Kopisi on 03 Sep, 2022.
+ * Contact Me : m.elkopisi@gmail.com
+ */
+
+@HiltAndroidApp
+class GithubApplication : Application() {
+  override fun onCreate() {
+    super.onCreate()
+  }
+}
--- presentation/src/main/java/me/melkopisi/githubreposlisting/MainActivity.kt
+++ presentation/src/main/java/me/melkopisi/githubreposlisting/MainActivity.kt
@@ -2,10 +2,13 @@ package me.melkopisi.githubreposlisting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import me.melkopisi.githubreposlisting.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(ActivityMainBinding.inflate(layoutInflater).root)
   }
 }
