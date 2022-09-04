df98ca1cf2847a73e84d4dca03ffd7cb699a5054
Kopisi <m.elkopisi@gmail.com>
2022-09-04 16:04:38 +0200

    made Ui Model parcelable for navigation

--- presentation/src/main/java/me/melkopisi/githubreposlisting/features/reposlist/models/GithubReposUiModel.kt
+++ presentation/src/main/java/me/melkopisi/githubreposlisting/features/reposlist/models/GithubReposUiModel.kt
@@ -1,9 +1,14 @@
 package me.melkopisi.githubreposlisting.features.reposlist.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 /*
  * Authored by Kopisi on 03 Sep, 2022.
  * Contact Me : m.elkopisi@gmail.com
  */
+
+@Parcelize
 data class GithubReposUiModel(
   val id: Int,
   val name: String,
@@ -12,8 +17,9 @@ data class GithubReposUiModel(
   val watchersCount: Int,
   val language: String?,
   val visibility: String,
-) {
+) : Parcelable {
+  @Parcelize
   data class Owner(
     val login: String,
-  )
+  ) : Parcelable
 }
